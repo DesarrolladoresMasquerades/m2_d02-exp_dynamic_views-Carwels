7f26c81d19187ecbf9138877ad1530d8b271ef4f
Carwi Barrios <carwelsbarrios@gmail.com>
2022-02-08 13:44:27 +0100

    class

--- app.js
+++ app.js
@@ -3,19 +3,23 @@ const express = require("express");
 require("dotenv").config();
 
 const app = express();
+// Telling the computer we want the dynamic not by hand
+app.set("view engine", "hbs");
+
+app.set("views", __dirname + "/views");
 
 app.use(express.static("public"));
 
+app.get("/home", (req, res, next) => res.render("home"));
 
-app.get("/home", (req, res, next) =>
-res.sendFile(__dirname + "/views/home.html")
+app.get("/about", (req, res, next) => res.render("about"));
+
+app.get("/", (req, res, next) =>
+  res.render("index", {
+    name: "Carwi",
+    hobbies: ["coding", "technology", "mountaneering"]
+  })
 );
-
-app.get("/about", (req, res, next) =>
-res.sendFile(__dirname + "/views/about.html")
-);
-
-app.get("/", (req, res, next) => res.send("Hello there!"));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("🏃‍ on port 3000"));
